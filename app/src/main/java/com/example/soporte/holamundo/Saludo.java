2dc8e57de8f56fdcd56049d8b4949c53530a5056
bryan <bryandaniel_14@hotmail.com>
2017-03-14 20:41:17 -0500

    otro commit aun mas

--- app/src/main/java/com/example/soporte/holamundo/Saludo.java
+++ app/src/main/java/com/example/soporte/holamundo/Saludo.java
@@ -1,5 +1,6 @@
 package com.example.soporte.holamundo;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -8,11 +9,21 @@ public class Saludo extends AppCompatActivity {
 
     private TextView saludo;
     private Bundle b;
-
+    private String aux, nom, apell;
+    private Resources res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
         saludo = (TextView)findViewById(R.id.txtSaludo);
+        b = getIntent().getExtras();
+        nom = b.getString("Nombre");
+        apell = b.getString("Apellido");
+        res = this.getResources();
+
+        aux= res.getString(R.string.parte_saludo)+" "+nom+" "+apell+" "+res.getString(R.string.parte_saludo2);
+        saludo.setText(aux);
+
+
     }
 }
